cc5c99e55b285e0bb11c0d2198af3bd1131167c8
karim076 <59794989+karim076@users.noreply.github.com>
2022-02-28 18:24:32 +0100

    Zonder comments

--- Neural-Network.py
+++ Neural-Network.py
@@ -1,17 +1,18 @@
 import os
 from pickletools import optimize
+from pyexpat import model
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-#mnist = tf.keras.datasets.mnist
+mnist = tf.keras.datasets.mnist
 
-#(x_train, y_train), (x_test, y_test) = mnist.load_data()#splitting data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()#splitting data
 
 # pixel has 0-255 values scale down to between 0 to 1
-#x_train = tf.keras.utils.normalize(x_train, axis=1)
-#x_test = tf.keras.utils.normalize(x_test, axis=1)
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 # creating model
 #model =  tf.keras.models.Sequential()
 #model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))# creating layer and flatten the input shape naar van 28 by 28px 
@@ -22,8 +23,9 @@ import tensorflow as tf
 #model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 
-#model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-#model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=3)
 
-#model.save('handwritten.model')
+model.save("Trained.Seq")
+
