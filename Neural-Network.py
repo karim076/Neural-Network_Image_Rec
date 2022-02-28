d0a6a63357793628e8cc8a6f351a08b4a29f0e0f
karim076 <59794989+karim076@users.noreply.github.com>
2022-02-28 18:33:02 +0100

    #1 fixed+nieuwe map "data training" dat is het opgeslagen data dat het neural network gebruikt

--- Neural-Network.py
+++ Neural-Network.py
@@ -1,6 +1,5 @@
 import os
 from pickletools import optimize
-from pyexpat import model
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,18 +13,18 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 # creating model
-#model =  tf.keras.models.Sequential()
-#model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))# creating layer and flatten the input shape naar van 28 by 28px 
+model =  tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))# creating layer and flatten the input shape naar van 28 by 28px 
 # dus we hebben 28 X 28 is 784 pixels 28, 28 is een grid net als 1920 bij 1080
 # nu gaan we 3 dense layers maken
-#model.add(tf.keras.layers.Dense(128, activation='relu'))
-#model.add(tf.keras.layers.Dense(128, activation='relu'))
-#model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=3)
 
-model.save("Trained.Seq")
+model.save("C:/Users/Karim Alkichouhi/Documents/School/Neural-Network/IMG-REC/Neural-Network_Image_Rec/tensorflow-demo/DataTraining")
 
